ff854fdedca5382b42ec74d2c572243071290cba
nachog16 <nachog16@hotmail.com>
2021-08-27 10:13:39 -0500

    python 2

--- nacho.py
+++
@@ -1,8 +0,0 @@
-# imprimir variables
-numero =100
-decimal = 3.5
-cadena = ("nacho")
-print ("vamos a imprimir mis variables ")
-print ("el numero menor de tres sifras es ", (numero))
-print ("la suma de 100 + 3.5, es igual a ",numero + decimal)
-print ("este es mi nombre", cadena)
---
+++ python/nacho.py
@@ -0,0 +1,10 @@
+# imprimir variables
+edad =0
+decimal = 0.0
+apellido= ("")
+inicial = ("")
+
+edad = ("introduzca su edad")
+
+
+
